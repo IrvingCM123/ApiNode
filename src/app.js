ceb1b61186228eaf49a0a44f803ff6b570964023
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-05-31 22:25:25 -0600

    cambios

--- src/app.js
+++ src/app.js
@@ -6,13 +6,12 @@ import morgan from "morgan";
 
 const app = express()
 app.set('port', config.port)
-
+ 
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false })); //Recibir datos desde formularios html
-app.use(express.json()); //Usar JSON
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.json()); 
 
-// Routes
 app.use(routes);
 
 export default app;
